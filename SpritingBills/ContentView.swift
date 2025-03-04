250c85f3d3d3ce80c31719ca5c42035510e63330
irukahaoruka <erika03301109@gmail.com>
2025-03-04 18:44:24 +0900

    ファイルの構成を変更した

---
+++ SpritingBills/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpritingBills
+//
+//  Created by 櫻井絵理香 on 2025/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
