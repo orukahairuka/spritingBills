46fc773d2c65e9a35c96ff6ac805e83cd9553ae8
irukahaoruka <erika03301109@gmail.com>
2025-03-08 17:26:58 +0900

    import chartView

---
+++ SpritingBills/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SpritingBills
+//
+//  Created by 櫻井絵理香 on 2025/03/05.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
